c445cf55f8baa005525ce766afbf4922f0c30f52
Paul Ronga <paul.ronga@laposte.net>
2020-03-23 12:13:53 +0100

    cache bypass

--- update/animatedBarChart.js
+++ update/animatedBarChart.js
@@ -47,7 +47,7 @@ $( document ).ready(function() {
   var yaxis = d3.axisLeft().scale(yscale);
   var g_yaxis = g.append("g").attr("class", "y axis");
 
-  d3.json("data/top10.json?1584888001259").then(json => {
+  d3.json("data/top10.json?1584957067459").then(json => {
     data = json;
     data.forEach(function(d) {
       if( availableDates.indexOf( d['date'] ) == -1 ){
